f968e02642645c2345c03b4c0178f364d1b5b11b
Aldo Resendiz <aldoresendiz@gmail.com>
2024-02-11 16:04:32 -0600

    Ready for review and feedback

--- functions_practice.py
+++ functions_practice.py
@@ -1,2 +1,26 @@
 def hello():
+    print("Hello World!")
 
+def pack(arg1, arg2, arg3):
+    lista = [arg1, arg2, arg3]
+    return(lista)
+
+def eat_lunch(my_list):
+    if not my_list:
+        print("My luchbox is empty!")
+    else:
+        for i in range(len(my_list)):
+            if i == 0: 
+                salida = "First "
+            else:
+                salida = "Next "
+            salida += "I eat " + my_list[i]
+            print(salida)
+
+print(hello())
+print("\n")
+print(pack("1","2","3"))
+print("\n")
+print(eat_lunch([]))
+print("\n")
+print(eat_lunch(["Ham", "Bacon", "Chorizo"]))
